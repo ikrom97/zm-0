d1023dbf96304c01541c15e79e023063614503d9
ikrom97 <ikromr04@gmail.com>
2023-05-12 12:15:53 +0500

    api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:3000/';
 const REQUEST_TIMEOUT = 5000;
 
 export const api = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: process.env.API_URL,
   timeout: REQUEST_TIMEOUT,
 });
